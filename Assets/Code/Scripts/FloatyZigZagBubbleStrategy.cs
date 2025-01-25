3414bdd2cdc4a62cce158003f9ab200e8a00c174
Brynn R <Brynn.rothuis@hotmail.com>
2025-01-25 15:23:45 +0100

    Spiral Bubble works
    Centre calculation goes wrong but its consistent

--- Assets/Code/Scripts/FloatyZigZagBubbleStrategy.cs
+++ Assets/Code/Scripts/FloatyZigZagBubbleStrategy.cs
@@ -4,38 +4,16 @@ using UnityEngine;
 public class FloatyZigZagBubbleStrategy : BubbleStrategyBase
 {
     [SerializeField] private int maxSwing = 1;
-    [SerializeField] private bool right = true;
-    private int currentSwing = 0;
     private Vector3 movement = Vector3.zero;
+    private int currentSwing = 0;
     
     public override void OnUpdate()
     {
         if(Bubble.HasVictim)
         {
-            if(right)
-            {
-                if(currentSwing != maxSwing)
-                {
-                    currentSwing++;
-                    movement = new Vector3(0, 1, currentSwing);
-                }
-                else
-                {
-                    right = false;
-                }
-            }
-            else
-            {
-                if (currentSwing != -maxSwing)
-                {
-                    currentSwing--;
-                    movement = new Vector3(0, 1, currentSwing);
-                }
-                else
-                {
-                    right = true;
-                }
-            }
+            float swingValue = Mathf.Sin(Time.time) * maxSwing;
+
+            movement = new Vector3(0, 1, swingValue);
 
             Bubble.transform.position += movement * Bubble.MoveSpeed;
         }
